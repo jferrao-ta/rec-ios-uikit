89875989758ced8a632448495964338534d665e0
Ricardo Frade <rfrade@tripadvisor.com>
2021-11-17 13:01:59 +0000

    Add project

---
+++ rec/RootViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RootViewController.swift
+//  rec
+//
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
